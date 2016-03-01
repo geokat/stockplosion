7b831c8226f2a40fbf37e1098b2e161cfa12b8b4
George Katsitadze <george.katsitadze@gmail.com>
2016-03-01 07:59:27 -0800

    Added CI with Travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
     singleRun: true,
     frameworks: ['mocha'],
     files: [
